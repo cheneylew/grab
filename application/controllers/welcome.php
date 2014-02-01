c661966ccc1b9a0dccfbe94734c001a9ad11b280
cheneylew <cheneylew@163.com>
2014-02-01 21:32:42 +0800

    add simple html dom parser to codeignitor as a application/helpers: how to make it work: http://ellislab.com/forums/viewthread/114059/
    Waldmeister is the master. Thanks for your persistence in getting me to try it as a helper, rather than a library, as my lack in understanding the subtle differences between a CI Library and CI Helper seems to have been the main problem for me getting it to work.
    
    I got it to work by doing the following steps:
    
    1) Drop the file simple_html_dom.php into /application/helpers folder
    2) Change the name to simple_html_dom_helper.php
    3) Capitalize the first letter of the two class names in the file:
    around line 83 class Simple_html_dom_node
    around line 481 class Simple_html_dom
    4) Then remember to load the new helper in the controller with $this->load->helper(‘simple_html_dom’)

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -19,7 +19,39 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		//$this->load->view('welcome_message');
+		$this->load->helper('simple_html_dom');
+		//Target site url
+		$targetUrl='http://sh.ganji.com/iphone-iphone-5/o3/';
+		$itemBaseUrl='http://sh.ganji.com';
+		
+		$html = file_get_html($targetUrl);
+		
+		header("Content-type:text/html;charset=utf-8;");
+
+	
+		$ret = $html->find('dl[class=list-bigpic]');
+		echo "<pre>";
+		foreach ($ret as $item){
+			$yuan = $item->children(2)->children(0)->children(0)->plaintext;
+			$title = $item->children(1)->children(0)->children(0)->children(0)->children(0)->plaintext;
+		
+			$link = $item->children(1)->children(0)->children(0)->children(0)->children(0);
+			$linkSite=$itemBaseUrl.$link->href;
+		
+			$content = $item->children(1)->children(1)->plaintext;
+			//$date = $item->children(2)->children(2)->plaintext;
+			//$location = $item->children(2)->children(3)->children(0)->plaintext;
+			//$date=$item->find('i[class=mr8]')->outertext;
+			$dates=$item->find('i[class=mr8]');
+			$date=$dates[0]->plaintext;
+		
+			$addses=$item->find('a[class=adds]');
+			$adds=$addses[0]->plaintext;
+		
+			echo "|$date |$adds|$yuan|$title |$linkSite |$content |<br/>";
+		}
+		echo count($ret);
 	}
 }
 
