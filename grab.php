21a653621ec69ef410cf304972a99f684c3b196f
cheneylew <cheneylew@163.com>
2014-02-01 17:41:21 +0800

    test origin reverse.

--- grab.php
+++ grab.php
@@ -1,3 +1,11 @@
 <?php
 include_once '3rdPartyClass/simple_html_dom.php';
-echo "hello world";
+header("Content-type:text/html;charset=utf-8;");
+$html = file_get_html('http://sh.ganji.com/iphone/o1/');
+$ret = $html->find('dl[class=list-bigpic]');
+echo "<pre>";
+foreach ($ret as $item){
+	$text=$item->plaintext;
+	echo "ok$text<br/>";
+}
+echo count($ret);
