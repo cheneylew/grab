faae5676221d3934fa5fd0b258ac0e6b16d56fe0
cheneylew <cheneylew@163.com>
2014-02-01 17:44:15 +0800

    Revert "test origin reverse."
    This reverts commit 21a653621ec69ef410cf304972a99f684c3b196f.

--- grab.php
+++ grab.php
@@ -1,11 +1,3 @@
 <?php
 include_once '3rdPartyClass/simple_html_dom.php';
-header("Content-type:text/html;charset=utf-8;");
-$html = file_get_html('http://sh.ganji.com/iphone/o1/');
-$ret = $html->find('dl[class=list-bigpic]');
-echo "<pre>";
-foreach ($ret as $item){
-	$text=$item->plaintext;
-	echo "ok$text<br/>";
-}
-echo count($ret);
+echo "hello world";
