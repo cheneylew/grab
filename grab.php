1a0dc642248f552f71e38a1c7c5b84716e11f759
cheneylew <cheneylew@163.com>
2014-02-01 21:06:43 +0800

    ganji site catch every message from list page.

--- grab.php
+++ grab.php
@@ -1,11 +1,30 @@
 <?php
 include_once '3rdPartyClass/simple_html_dom.php';
 header("Content-type:text/html;charset=utf-8;");
-$html = file_get_html('http://sh.ganji.com/iphone/o1/');
+//Target site url
+$targetUrl='http://sh.ganji.com/iphone-iphone-5/o3/';
+$itemBaseUrl='http://sh.ganji.com';
+
+$html = file_get_html($targetUrl);
 $ret = $html->find('dl[class=list-bigpic]');
 echo "<pre>";
 foreach ($ret as $item){
-	$text=$item->plaintext;
-	echo "ok$text<br/>";
+	$yuan = $item->children(2)->children(0)->children(0)->plaintext;
+	$title = $item->children(1)->children(0)->children(0)->children(0)->children(0)->plaintext;
+	
+	$link = $item->children(1)->children(0)->children(0)->children(0)->children(0);
+	$linkSite=$itemBaseUrl.$link->href;
+	
+	$content = $item->children(1)->children(1)->plaintext;
+	//$date = $item->children(2)->children(2)->plaintext;
+	//$location = $item->children(2)->children(3)->children(0)->plaintext;
+	//$date=$item->find('i[class=mr8]')->outertext;
+	$dates=$item->find('i[class=mr8]');
+	$date=$dates[0]->plaintext;
+	
+	$addses=$item->find('a[class=adds]');
+	$adds=$addses[0]->plaintext;
+	
+	echo "$content<br/>";
 }
 echo count($ret);
